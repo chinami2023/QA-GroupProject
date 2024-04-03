3b657177ecd215d4e2c885e3781a4910bdf40f1c
chinami2023 <bcit.ch2023@gmail.com>
2024-04-02 17:24:00 -0700

    database changed

--- qa-project/app/database/connect.php
+++ qa-project/app/database/connect.php
@@ -1,11 +1,39 @@
 <?php
 
-$user = "root";
-$pass = "root";
+// $user = "root";
+// $pass = "root";
+
+$servername = "localhost";
+$username = "china406_dbzuser";
+$password = "qa-admin";
+$db_name = "china406_dbz";
+
+// $conn = new mysqli($servername,$username,$password,$db_name);
+
+$conn = new mysqli($servername,$username, $password, $db_name);
 
 try{
-    $pdo = new PDO('mysql:host=localhost;dbname=dbz', $user, $pass);
+    $pdo = new PDO('mysql:host=localhost;dbname=china406_dbz', $username, $password);
     // echo "you successed DB";
 }catch(PDOException $error){
 echo $erro->getMessage();
 }
+
+
+
+
+// 
+
+// define('_HOSTNAME', 'localhost');
+// define('_DAYEBASE_NAME', 'china406_dbz');
+// define('_DAYEBASE_USER_NAME', 'china406_dbzuser');
+// define('_DATABASE_PASSWORD', 'ContactDatabase');
+
+// $MySQLicon = new MySQLi(_HOSTNAME, _DAYEBASE_NAME,_DAYEBASE_USER_NAME,_DATABASE_PASSWORD);
+
+// if($MyAQLiconn->connect_errno){
+//     dei('ERROR:' .$mySQLiconn->connect_error);
+// }
+
+
+
