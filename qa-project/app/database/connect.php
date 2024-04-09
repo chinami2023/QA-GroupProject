e7d97e7ea65f558d8146bba184fda4dd5d1c9677
Chinashima <149334664+chinami2023@users.noreply.github.com>
2024-04-09 11:40:51 -0700

    Add files via upload

--- qa-project/app/database/connect.php
+++ qa-project/app/database/connect.php
@@ -1,39 +1,11 @@
 <?php
 
-// $user = "root";
-// $pass = "root";
-
-$servername = "localhost";
-$username = "china406_dbzuser";
-$password = "qa-admin";
-$db_name = "china406_dbz";
-
-// $conn = new mysqli($servername,$username,$password,$db_name);
-
-$conn = new mysqli($servername,$username, $password, $db_name);
+$user = "root";
+$pass = "root";
 
 try{
-    $pdo = new PDO('mysql:host=localhost;dbname=china406_dbz', $username, $password);
+    $pdo = new PDO('mysql:host=localhost;dbname=dbz', $user, $pass);
     // echo "you successed DB";
 }catch(PDOException $error){
 echo $erro->getMessage();
 }
-
-
-
-
-// 
-
-// define('_HOSTNAME', 'localhost');
-// define('_DAYEBASE_NAME', 'china406_dbz');
-// define('_DAYEBASE_USER_NAME', 'china406_dbzuser');
-// define('_DATABASE_PASSWORD', 'ContactDatabase');
-
-// $MySQLicon = new MySQLi(_HOSTNAME, _DAYEBASE_NAME,_DAYEBASE_USER_NAME,_DATABASE_PASSWORD);
-
-// if($MyAQLiconn->connect_errno){
-//     dei('ERROR:' .$mySQLiconn->connect_error);
-// }
-
-
-
